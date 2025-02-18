58bf31b3a2c694fa4cd01dfe6c37e2bba5ab4ab6
davidj-therobot <davidj@therobot.network>
2025-02-18 12:43:26 +0200

    removed 'Primitive' from plan step in favor of address db lookup

---
+++ precompile/contracts/llmprecompile/contractsAddresses.go
@@ -0,0 +1,9 @@
+package llmprecompile
+
+
+var contractsAddresses = map[string]string{
+  "0x17aB05351fC94a1a67Bf3f56DdbB941aE6c63E25": "counter",
+  "0x5aa01B3b5877255cE50cc55e8986a7a5fe29C70e": "counter",
+  "0x5DB9A7629912EBF95876228C24A848de0bfB43A9": "erc20",
+  "0x4Ac1d98D9cEF99EC6546dEd4Bd550b0b287aaD6D": "math",
+}
--- precompile/contracts/llmprecompile/demoPlans.go
+++ precompile/contracts/llmprecompile/demoPlans.go
@@ -8,7 +8,6 @@ var demoPlans = map[string][]Step{
 		Contract: Arg{
 			Lookup: "USDC" ,
 		},
-		Primitive: "counter",
 		Args: []Arg{
 			{
 				Value: "10",
@@ -20,7 +19,6 @@ var demoPlans = map[string][]Step{
 		Contract: Arg{
 			Value: "0x5aa01B3b5877255cE50cc55e8986a7a5fe29C70e",
 		},
-		Primitive: "counter",
 		Args: []Arg{
 			{
 				Value: "20",
@@ -34,7 +32,6 @@ var demoPlans = map[string][]Step{
       Contract: Arg{
 				Value: "0x17aB05351fC94a1a67Bf3f56DdbB941aE6c63E25",
 			},
-      Primitive: "counter",
 	  Args: []Arg{
         {
           Value: "10",
@@ -46,7 +43,6 @@ var demoPlans = map[string][]Step{
 			Contract: Arg{
 				Value: "0x17aB05351fC94a1a67Bf3f56DdbB941aE6c63E25",
 			},
-			Primitive: "counter",
 			Args: []Arg{},
 			Output: "CountA",
 		},
@@ -55,7 +51,6 @@ var demoPlans = map[string][]Step{
 			Contract: Arg{
 				Value: "0x5aa01B3b5877255cE50cc55e8986a7a5fe29C70e",
 			},
-			Primitive: "counter",
 			Args: []Arg{
 				{
 					Lookup: "CountA",
@@ -71,28 +66,7 @@ var demoPlans = map[string][]Step{
 			Contract: Arg{
 				Value: "0x52C84043CD9c865236f11d9Fc9F56aa003c1f922",
 			},
-			Primitive: `[
-				{
-					"inputs": [
-						{
-							"internalType": "address",
-							"name": "account",
-							"type": "address"
-						}
-					],
-					"name": "balanceOf",
-					"outputs": [
-						{
-							"internalType": "uint256",
-							"name": "",
-							"type": "uint256"
-						}
-					],
-					"stateMutability": "view",
-					"type": "function"
-				}
-			]`,
-			Args: []Arg{
+				Args: []Arg{
 				{
 					Value: "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC",
 				},
@@ -103,33 +77,7 @@ var demoPlans = map[string][]Step{
 			Contract: Arg{
 				Value: "0xC6d7eF1e8BEd05586A46Bef5e1E392DF64070503",
 			},
-			Primitive: `[
-				{
-					"inputs": [
-						{
-							"internalType": "uint256",
-							"name": "a",
-							"type": "uint256"
-						},
-						{
-							"internalType": "uint256",
-							"name": "b",
-							"type": "uint256"
-						}
-					],
-					"name": "divide",
-					"outputs": [
-						{
-							"internalType": "uint256",
-							"name": "",
-							"type": "uint256"
-						}
-					],
-					"stateMutability": "pure",
-					"type": "function"
-				}
-			]`,
-			Args: []Arg{
+				Args: []Arg{
 				{
 					Lookup: "hello",
 				},
@@ -143,32 +91,6 @@ var demoPlans = map[string][]Step{
 			Contract: Arg{
 				Value: "0xC6d7eF1e8BEd05586A46Bef5e1E392DF64070503",
 			},
-			Primitive: `[
-				{
-					"inputs": [
-						{
-							"internalType": "uint256",
-							"name": "a",
-							"type": "uint256"
-						},
-						{
-							"internalType": "uint256",
-							"name": "b",
-							"type": "uint256"
-						}
-					],
-					"name": "isLessThanOrEqual",
-					"outputs": [
-						{
-							"internalType": "bool",
-							"name": "",
-							"type": "bool"
-						}
-					],
-					"stateMutability": "pure",
-					"type": "function"
-				}
-			]`,
 			Args: []Arg{
 				{
 					Lookup: "hello",
@@ -186,7 +108,6 @@ var demoPlans = map[string][]Step{
 		},
 		{
 			Method:   "jumpIf",
-			Primitive: "GoMethods",
 			Args: []Arg{
 				{
 					Value:  "true",
@@ -201,33 +122,7 @@ var demoPlans = map[string][]Step{
 			Contract: Arg{
 				Value: "0x52C84043CD9c865236f11d9Fc9F56aa003c1f922",
 			},
-			Primitive: `[
-				{
-					"inputs": [
-						{
-							"internalType": "address",
-							"name": "to",
-							"type": "address"
-						},
-						{
-							"internalType": "uint256",
-							"name": "amount",
-							"type": "uint256"
-						}
-					],
-					"name": "transfer",
-					"outputs": [
-						{
-							"internalType": "bool",
-							"name": "",
-							"type": "bool"
-						}
-					],
-					"stateMutability": "nonpayable",
-					"type": "function"
-				}
-			]`,
-			Args: []Arg{
+				Args: []Arg{
 				{
 					Value:  "0xad660da80c8D32E1a4Fb8DF6925A428060b58616",
 				},
