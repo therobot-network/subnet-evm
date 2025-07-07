66bd180b71383969801820701d0fca05dd9cf9af
davidj-therobot <davidj@therobot.network>
2025-07-07 13:18:39 +0300

    added commented out build config for debugging process

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,3 +28,8 @@ fi
 # Build Subnet EVM, which is run as a subprocess
 echo "Building Subnet EVM @ GitCommit: $SUBNET_EVM_COMMIT at $BINARY_PATH"
 go build -ldflags "-X github.com/ava-labs/subnet-evm/plugin/evm.GitCommit=$SUBNET_EVM_COMMIT $STATIC_LD_FLAGS" -o "$BINARY_PATH" "plugin/"*.go
+
+# go build \
+#   -gcflags="all=-N -l" \
+#   -ldflags "-X github.com/ava-labs/subnet-evm/plugin/evm.GitCommit=$SUBNET_EVM_COMMIT $STATIC_LD_FLAGS" \
+#   -o "$BINARY_PATH" "plugin/"*.go
